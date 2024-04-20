ee5f94aad9d8d0777de762f3111509dde838f0e0
HarryP23 <132031418+HarryP23@users.noreply.github.com>
2024-04-20 13:28:04 +0900

    0420 1:27pm

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,9 +1,43 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const x = 23;
-if (x === 3) console.log(23);
+// const x = 23;
+// if (x === 3) console.log(23);
 
-const calcAge = (birthYear) => 2037 - birthYear;
+// const calcAge = (birthYear) => 2037 - birthYear;
 
-console.log(calcAge(1991));
+// console.log(calcAge(2002));
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    // value: prompt("Degrees celsius:"),
+    value: Number(prompt("Degrees celsius:")),
+  };
+
+  console.table(measurement);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+// console.log(measureKelvin());
+// console.warn(measureKelvin());
+// console.error(measureKelvin());
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+const testdata1 = [17, 21, 23];
+const testdata2 = [12, 5, -5, 0, 4];
+
+const printForecast = function () {};
--- 05-Guess-My-Number/mine/script.js
+++ 05-Guess-My-Number/mine/script.js
@@ -1,3 +1,4 @@
 "use strict";
 
 console.log(321);
+console.log("");
